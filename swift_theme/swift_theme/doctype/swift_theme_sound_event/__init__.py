a1bde30e9ae05ed66001e4f543985e744a8b7530
Ali Raza <iamaliraza777@gmail.com>
2026-07-31 18:57:14 +0500

    feat: Implement dynamic theming and glassmorphism design for Enterprise Login

--- swift_theme/swift_theme/doctype/swift_theme_sound_event/__init__.py
+++ swift_theme/swift_theme/doctype/swift_theme_sound_event/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2025, its-alikhokher
 # License: MIT
 
-from .swift_theme_sound_event import SoundEvent
+from .swift_theme_sound_event import SwiftThemeSoundEvent
 
-__all__ = ["SoundEvent"]
+__all__ = ["SwiftThemeSoundEvent"]
